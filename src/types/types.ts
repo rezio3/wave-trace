67c432fbcaf4fdd74e296e5c23a4fd8962503402
Joszko <jakub.rezler96@gmail.com>
2024-04-18 16:42:32 +0200

    format code in types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,23 +3,23 @@ import { store } from "../app/store";
 export type AppDispatch = typeof store.dispatch;
 
 export type UserReturnState = {
-    data: {
-      user: InitialState;
-    }
-  }
-  
-  export type UserDetails = {
-    uid?: string
-    userName: string | null
-    email?: string | null
-  }
-  
-  export type InitialState = {
-    user: UserDetails
-    isLoading: Boolean
-  }
-  
-  export const initialState: InitialState = {
-    user: {userName: ""},
-    isLoading: true,
+  data: {
+    user: InitialState;
   };
+};
+
+export type UserDetails = {
+  uid?: string;
+  userName: string | null;
+  email?: string | null;
+};
+
+export type InitialState = {
+  user: UserDetails;
+  isLoading: Boolean;
+};
+
+export const initialState: InitialState = {
+  user: { userName: "" },
+  isLoading: true,
+};
